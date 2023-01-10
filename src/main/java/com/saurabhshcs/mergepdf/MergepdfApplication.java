8362ca674c218b7778943dec4d4e7a35fc2c9e27
saurabhshcs <saurabhshcs@gmail.com>
2023-01-10 23:22:08 +0000

    saurabhshcs-01:merge-pdf: Initial project structure commit.

---
+++ src/main/java/com/saurabhshcs/mergepdf/MergepdfApplication.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.mergepdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergepdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergepdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabhshcs/mergepdf/MergepdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.mergepdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergepdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
